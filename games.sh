9bb19b67ffc1bb85ff2e0d2d9815c2044fef95a4
Ephantus Macharia <60391244+gem-404@users.noreply.github.com>
2023-02-14 03:51:40 +0300

    Added a comment to games script

--- games.sh
+++ games.sh
@@ -3,5 +3,6 @@
 file=./games.txt
 
 
-cat games.txt | xargs -I {} curl -s -o games2.html {}
+# Runs the curl command silently, and outputs the results to games2.html
 
+cat games.txt | xargs -I {} curl -s -o games2.html {}
