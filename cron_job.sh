4ec889aa0b9860e07a6ce77467f25b5fd99fb4d2
GEM-404 <mashaephantus2000@gmail.com>
2023-02-20 06:41:41 +0300

    Added code to remove lines that are not chess urls

--- cron_job.sh
+++ cron_job.sh
@@ -5,5 +5,14 @@ file=games.txt
 # number of lines the file has
 lines=`cat $file | wc -l`
 
+# Check for lines that do not conform to the pattern, https://www.chess.com/game/live/69853418297,
+# and remove them.
+sed -i '/^https:\/\/www\.[a-z]+\.[a-z]+\/[a-z]+\/[a-z]+\/[0-9]\+$/!d' $file
+
+# Check for duplicate lines and remove them
+cat $file | sort -u | uniq > $file
 
 # Check if any new lines have been added since last time
+
+
+# Automatically add, commit and push the changed file to the git repo
